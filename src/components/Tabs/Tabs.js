514fd7c7b0be1f2f3a27cb91a7feff83f05ca173
3ndrius <andrzejo1991@gmail.com>
2020-02-13 11:14:24 +0100

    Finish v1

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,34 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './tabs.scss';
 import Img from '../../assets/images/oval_3.png';
 
 export default function Tabs() {
+
+    const [active, setActive] = useState(false);
+
+    const handleTabs = () => {
+        setActive(!active);
+    }
     return (
         <section className="tabs">
             <header className="tabs__header">
-                <ul className="tabs__list">
-                    <li className="tabs__item tabs__item--active">
+                <ul className="tabs__list" onClick={handleTabs}>
+                    <li className="tabs__item" style={active? {} : {borderBottom: '2px solid #2b8ff4'}}>
                         Request
                     </li>
-                    <li className="tabs__item ">
+                    <li className="tabs__item " style={active? {borderBottom: '2px solid #2b8ff4'} : {}}>
                         Activity Score
                     </li>
                 </ul>
             </header>
-            <div className="tabs__wrap">
-            
-              <table>
-                    <th>
-                        <tr>
-                       <h2> Description</h2>
-                        <h2>Status</h2>
-                        <h2>Due date</h2>
-                        <h2>Responsible party</h2>
-                        <h2>Send income</h2>
-                       <h2> More</h2>
-                        </tr>
-                    </th>
-                    <tbody>
+            <div className="tabs__activity" style={active? {display: 'block'} : {display:'none'}}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis culpa sapiente sequi cumque vitae aperiam pariatur repellendus alias quis vel, voluptates hic qui quibusdam, recusandae error. Dicta ipsa magni eaque, culpa dolorem sequi quidem itaque labore accusantium voluptate nisi dolor perspiciatis minima, maxime numquam in repudiandae quae! Eligendi omnis ad beatae at error minima repellendus quasi debitis a? Modi animi dolorum vitae, aspernatur aliquam voluptatibus voluptatem nemo iste, doloremque quo ab! Cupiditate officiis totam assumenda modi voluptatibus maxime aperiam, fugit aspernatur quia, veritatis, dolorem sunt vitae perspiciatis quae quasi? Quibusdam, alias vel! Nesciunt saepe rerum quidem consectetur, et quam blanditiis.
+                </div>
+            <div className="tabs__wrap" style={active? {display: 'none'} : {display:'block'}}>
+              <table className="tabs-table">
+                  <thead className="tabs-table__header">
+                    <tr>
+                       <td > Description</td>
+                        <td >Status</td>
+                        <td >Due date</td>
+                        <td >Responsible party</td>
+                        <td >Send income</td>
+                       <td > More</td>
+                    </tr>
+                    </thead>
+                    <tbody className="tabs-table__body">
                         <tr>
                             <td>Lorem ipsum dolor</td>
                             <td>Solved</td>
@@ -55,6 +63,7 @@ export default function Tabs() {
                         </tr>
                     </tbody>
                     
+                   
                 </table>
             </div>
         </section>
